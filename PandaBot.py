8e919573132f534e4351d18b97a7409310c22b5d
dktlseh12345 <48846842+dktlseh12345@users.noreply.github.com>
2021-09-21 17:50:04 +0900

    Update PandaBot.py

--- PandaBot.py
+++ PandaBot.py
@@ -189,10 +189,16 @@ async def 즐겨찾기(ctx):
 
 @bot.event
 async def on_ready():
-    print('다음으로 로그인합니다: ')
-    print(bot.user.name)
-    print('connection was succesful')
-    await bot.change_presence(status=discord.Status.online, activity=discord.Game("봇 코딩중~"))
+
+  # [discord.Status.online = 온라인],[discord.Status.idle = 자리비움],[discord.Status.dnd = 다른용무],[discord.Status.offline = 오프라인]
+  await client.change_presence(status=discord.Status.online)
+
+  await client.change_presence(activity=discord.Game(name="봇 코딩중"))
+  #await client.change_presence(activity=discord.Streaming(name="스트림 방송중", url='링크'))
+  #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="노래 듣는중"))
+  #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="영상 시청중"))
+  
+  print("봇 이름:",client.user.name,"봇 아이디:",client.user.id,"봇 버전:",discord.__version__)
 
 @bot.command()
 async def 입장(ctx):
